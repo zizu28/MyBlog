86f9af9f339a1007e17f09fe316b0fc6c93ed9d4
Zulka <ziblimzulka.zz@gmail.com>
2023-06-10 16:06:46 +0700

    Add project files.

---
+++ Data.Models/Models/BlogPost.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models.Models
+{
+    public class BlogPost
+    {
+        public string? Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Text { get; set; } = string.Empty;
+    }
+}

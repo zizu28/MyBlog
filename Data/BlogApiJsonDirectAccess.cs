5e4b506e4528939e6e7de7e6b56e8a2fa8ef9bc1
Zulka <ziblimzulka.zz@gmail.com>
2023-06-10 16:43:47 +0700

    Add class to read settings from blogpost, categories and tags

---
+++ Data/BlogApiJsonDirectAccess.cs
@@ -0,0 +1,101 @@
+﻿using Data.Models.Interfaces;
+using Data.Models.Models;
+using Microsoft.Extensions.Options;
+using System.Text.Json;
+
+namespace Data
+{
+    public class BlogApiJsonDirectAccess : IBlogApi
+    {
+        BlogApiJsonDirectAccessSetting _option;
+
+        public BlogApiJsonDirectAccess(IOptions<BlogApiJsonDirectAccessSetting> option)
+        {
+            _option = option.Value;
+
+            if (!Directory.Exists(_option.DataPath))
+            {
+                Directory.CreateDirectory(_option.DataPath);
+            }
+
+            if (!Directory.Exists($@"{_option.DataPath}\{_option.BlogPostsFolder}"))
+            {
+                Directory.CreateDirectory($@"{_option.DataPath}\{_option.BlogPostsFolder}");
+            }
+
+            if (!Directory.Exists($@"{_option.DataPath}\{_option.CategoriesFolder}"))
+            {
+                Directory.CreateDirectory($@"{_option.DataPath}\{_option.CategoriesFolder}");
+            }
+
+            if (!Directory.Exists($@"{_option.DataPath}\{_option.TagsFolder}"))
+            {
+                Directory.CreateDirectory($@"{_option.DataPath}\{_option.TagsFolder}");
+            }
+        }
+        public Task<BlogPost?> DeleteBlogPostAsync(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Category?> DeleteCategoryAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tag?> DeletTagAsync(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BlogPost?> GetBlogPostByIdAsync(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> GetBlogPostCountAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BlogPost>?> GetBlogPostsAsync(int numberOfPosts, int startIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Category>?> GetCategoriesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Category?> GetCategoryByIdAsync(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tag?> GetTagByIdAsync(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Tag>?> GetTagsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BlogPost?> SaveBlogPostAsync(BlogPost post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Category?> SaveCategoryAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tag?> SaveTagAsync(Tag tag)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Data/BlogApiJsonDirectAccessSetting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class BlogApiJsonDirectAccessSetting
+    {
+        public string BlogPostsFolder { get; set; } = string.Empty;
+        public string CategoriesFolder { get; set; } = string.Empty;
+        public string TagsFolder { get; set; } = string.Empty;
+        public string DataPath { get; set; } = string.Empty;
+    }
+}

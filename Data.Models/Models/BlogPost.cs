a3f0ee4164614c729c39800a8eebb29cc91a60ff
Zulka <ziblimzulka.zz@gmail.com>
2023-06-10 16:09:52 +0700

    Create models for blogpost, category and tags

--- Data.Models/Models/BlogPost.cs
+++ Data.Models/Models/BlogPost.cs
@@ -11,5 +11,8 @@ namespace Data.Models.Models
         public string? Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Text { get; set; } = string.Empty;
+        public DateTime PublishedDate { get; set; }
+        public Category? Category { get; set; }
+        public List<Tag> Tags { get; set; } = new();
     }
 }
